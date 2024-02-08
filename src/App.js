6787e64b0bfbd7ed116df556051a5fd9829c674f
Shivangi Sharma <shanussh@gmail.com>
2024-02-08 23:29:16 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import Register from "./components/Register";
+import ipConfig from "./ipConfig.json";
+import { Route, Switch } from "react-router-dom";
+import Login from "./components/Login";
+import Products from "./components/Products";
+
+export const config = {
+  endpoint: `http://localhost:3000/api/v1`,
+};
+
+function App() {
+  return (
+    <div className="App">
+      {/* TODO: CRIO_TASK_MODULE_LOGIN - To add configure routes and their mapping */}
+      {/* <Register /> */}
+
+      <Switch>
+        <Route path="/register">
+          <Register />
+        </Route>
+        <Route path="/login">
+          <Login />
+        </Route>
+        <Route path="/">
+          <Products />
+        </Route>
+      </Switch>
+    </div>
+  );
+}
+
+export default App;
